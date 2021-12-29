820c3d57e459d1ec11a5d7de9f4ee7d5f3e43b6a
Yuqifan1117 <1138414330@qq.com>
2021-12-29 16:37:27 +0800

    alexnet commit

--- homework3/AlexNet.py
+++ homework3/AlexNet.py
@@ -131,11 +131,11 @@ def main():
     # load cifar10 dataset
     transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=False, transform=transform)
+                                        download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
                                             shuffle=True, num_workers=4)
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                        download=False, transform=transform)
+                                        download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, 
                                             shuffle=False, num_workers=4)
     classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
